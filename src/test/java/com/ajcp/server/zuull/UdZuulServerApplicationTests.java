17d1ba044f0bf4d96edf574b2c2c26e60fa8caa7
andresjc15 <andres.jcp15@gmail.com>
2022-08-13 09:35:54 -0500

    primera carga

---
+++ src/test/java/com/ajcp/server/zuull/UdZuulServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajcp.server.zuull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdZuulServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
